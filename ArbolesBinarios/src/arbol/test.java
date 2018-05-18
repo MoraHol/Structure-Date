c7dc426a270ab01ed6d1776ce44c8888fb7e185f
alexis holguin <33912362+MoraHol@users.noreply.github.com>
2018-05-18 11:18:30 -0500

    Update test.java

--- ArbolesBinarios/src/arbol/test.java
+++ ArbolesBinarios/src/arbol/test.java
@@ -7,19 +7,22 @@ public class test {
 		for (int i = 0; i < nodos.length; i++) {
 			arbol.addNodo(nodos[i]);
 		}
-		System.out.println(arbol.CountNodes());
+		System.out.println("En este nodo hay: "+arbol.CountNodes());
+		System.out.println("arbol en inorden: ");
 		arbol.inorden();
+		System.out.println("Arbol en preorden:");
 		System.out.println();
 		arbol.preorden();
+		System.out.println("Arbol en postorden: ");
 		System.out.println();
 		arbol.postorden();
-		System.out.println();
-		System.out.println(arbol.countInternalNodes());
-		System.out.println();
-		System.out.println(arbol.sumaNodos());
-		System.out.println();
-		System.out.println(arbol.promedio());
-		System.out.println();
-		System.out.println(arbol.maxValue());
+		System.out.println("En este arbo hay "+arbol.countInternalNodes()+" internos.");
+		
+		
+		System.out.println("la suma de todos los nodos del arbol es : "+arbol.sumaNodos());
+		
+		System.out.println("el promedio de los numeros de este arbol es: "+arbol.promedio());
+	
+		System.out.println("el numero mas grande de este arbol es: "+arbol.maxValue());
 	}
 }
