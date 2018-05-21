8a16f500338318d11f2c89c7eb8ecd614ae7bb33
MoraHol <holguinalexis30@gmail.com>
2018-05-20 20:48:36 -0500

    termina e interfaz

--- Puzzle/src/Puzzle/Puzzle2.java
+++ Puzzle/src/Puzzle/Puzzle2.java
@@ -7,65 +7,59 @@ import java.util.Stack;
 
 import Arbol.Nodo;
 
-public class Pluzze2 {
-	static int n =4;
-	static final int[][] solucion = MakeSolution(n);
+public class Puzzle2 {
+	private int[][] problema;
+	private Stack<Nodo> pasos;
+	private int n;
+	private int[][] solucion;
+	private int movimientos;
 
-	public static void main(String[] args) {
-
-		//int[][] inicio = RandomFillTokens(n);
-		//int[][] inicio = { { 8, 1, 2 }, {7, 6, 4 }, { 0, 5, 3 } };
-		//int[][] inicio = { { 1, 6, 2 }, {5, 8, 7 }, { 4, 0, 3 } };
-		int[][] inicio = { { 4, 14, 3, 7 }, { 1, 10, 8, 0 }, { 9, 11, 2, 12 }, { 13, 6, 5, 15 } };
-		
-		System.out.println("sin solucionar");
-		printMatriz(inicio);
-		System.out.println("solucion");
-		printMatriz(solucion);
-
-		Nodo sol = findSolution(inicio, solucion, n);
-		Stack<Nodo> pasos = new Stack<>();
-		System.out.println("pasos:");
-		printMatriz(inicio);
-		System.out.println();
+	public Puzzle2(int n) {
+		movimientos = 0;
+		this.n = n;
+		solucion = MakeSolution(n);
+		problema = RandomFillTokens(n);
+		Nodo sol = findSolution(problema, solucion, n);
+		pasos = new Stack<>();
 		while (sol.getPadre() != null) {
 			pasos.add(sol);
 			sol = sol.getPadre();
+			movimientos += 1;
 		}
-		int count = 0;
-		while (!pasos.isEmpty()) {
-			printMatriz(pasos.pop().getInfo());
-			System.out.println();
-			count++;
-		}
-		System.out.println(count);
 
 	}
 
-	static void printMatriz(int[][] matriz) {
-		for (int i = 0; i < matriz.length; i++) {
-			for (int j = 0; j < matriz.length; j++) {
-				System.out.print("[" + matriz[i][j] + "]");
-			}
-			System.out.println("");
-		}
+	public int getMovimientos() {
+		return movimientos;
 	}
 
-	static Nodo findSolution(int[][] problem, int[][] solution, int n) {
+	public Stack<Nodo> getPasos() {
+		return pasos;
+	}
+
+	public int[][] getProblema() {
+		return problema;
+	}
+
+	public int[][] getSolution() {
+		return solucion;
+	}
+
+	private Nodo findSolution(int[][] problem, int[][] solution, int n) {
 		Nodo revisar = new Nodo(problem);
 		ArrayList<Nodo> Recorridos = new ArrayList<>();
 		int menorCoste;
 		ArrayList<Nodo> visitados = new ArrayList<>();
-		int count = 0;
+		// int count = 0;
 		while (true) {
 			visitados.add(revisar);
-			
+
 			if (Arrays.deepEquals(revisar.getInfo(), solution)) {
-				System.out.println(count);
+				// System.out.println(count);
 				return revisar;
 			}
 			int posicion = -1;
-			ArrayList<Nodo> hijos = generateChildren(revisar, n,visitados);
+			ArrayList<Nodo> hijos = generateChildren(revisar, n, visitados);
 			for (Nodo hijo : hijos) {
 				Recorridos.add(hijo);
 				if (hijo.getPadre().getPadre() != null) {
@@ -83,18 +77,18 @@ public class Pluzze2 {
 			menorCoste = Integer.MAX_VALUE;
 			for (Nodo nodo : Recorridos) {
 				int auxCost = minCost(nodo, n);
-				if (auxCost < menorCoste ) {
+				if (auxCost < menorCoste) {
 					revisar = nodo;
 					menorCoste = auxCost;
 				}
 			}
-			
+
 			Recorridos.remove(revisar);
-			count++;
+			// count++;
 		}
 	}
 
-	static ArrayList<Nodo> generateChildren(Nodo revisar, int n,ArrayList<Nodo> visitados) {
+	private ArrayList<Nodo> generateChildren(Nodo revisar, int n, ArrayList<Nodo> visitados) {
 		int[] pCero = ubicarPosicionCero(revisar.getInfo());
 		ArrayList<Nodo> hijos = new ArrayList<>();
 		if (pCero[0] != 0) {
@@ -137,11 +131,11 @@ public class Pluzze2 {
 		return hijos;
 	}
 
-	static int minCost(Nodo x, int n) {
+	private int minCost(Nodo x, int n) {
 		return fatherDistance(x) + uncorredPosition(x, n);
 	}
 
-	static int fatherDistance(Nodo x) {
+	private int fatherDistance(Nodo x) {
 		int counter = 0;
 		Nodo aux = x;
 		while (aux.getPadre() != null) {
@@ -151,7 +145,7 @@ public class Pluzze2 {
 		return counter;
 	}
 
-	static int uncorredPosition(Nodo x, int n) {
+	private int uncorredPosition(Nodo x, int n) {
 		int[][] ordenada = solucion;
 		int counter = 0;
 		for (int i = 0; i < ordenada.length; i++) {
@@ -164,7 +158,7 @@ public class Pluzze2 {
 		return counter;
 	}
 
-	static int[] ubicarPosicionCero(int[][] matriz) {
+	private int[] ubicarPosicionCero(int[][] matriz) {
 		int[] vectorCero = new int[2]; // guardar la posición de cero
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz.length; j++) {
@@ -179,7 +173,7 @@ public class Pluzze2 {
 		return vectorCero;
 	}
 
-	static int[][] MakeSolution(int n) {
+	public static int[][] MakeSolution(int n) {
 		int aux = 1;
 		int[][] solucion = new int[n][n];
 		for (int i = 0; i < n; i++) {
@@ -197,12 +191,13 @@ public class Pluzze2 {
 
 	}
 
-	static int[][] RandomFillTokens(int n) {
+	private int[][] RandomFillTokens(int n) {
+		int aux = n == 3 ? 0 : n == 4 ? 4 : n == 5 ? 6 : 8;
 		int[][] Ordenada = clone(solucion);
 		Random rd = new Random();
 		int[] pCero = ubicarPosicionCero(Ordenada);
 		Nodo x = new Nodo(Ordenada);
-		while(uncorredPosition(x, n) != (n*n)) {
+		while (uncorredPosition(x, n) != (n * n) - aux) {
 			x.setInfo(Ordenada);
 			pCero = ubicarPosicionCero(Ordenada);
 			switch (rd.nextInt(4)) {
@@ -242,7 +237,7 @@ public class Pluzze2 {
 		return Ordenada;
 	}
 
-	static int[][] clone(int[][] matriz) {
+	private int[][] clone(int[][] matriz) {
 		int[][] clon = new int[matriz.length][matriz.length];
 		for (int i = 0; i < matriz.length; i++) {
 			clon[i] = matriz[i].clone();
---
+++ Puzzle/src/gui/InterfazPuzzle.java
@@ -0,0 +1,246 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JComboBox;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.DefaultComboBoxModel;
+import java.awt.GridLayout;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+import Puzzle.Puzzle2;
+import javax.swing.JButton;
+
+public class InterfazPuzzle extends JFrame implements ActionListener {
+	private Puzzle2 puzzle;
+	private ArrayList<int[][]> pasos;
+	private int n;
+	private int x = 0;
+	private static final String GENERAR_PROBLEMA = "generar problema";
+	private static final String GENERAR = "generar puzzle";
+	private static final String NEXTSTEP = "siguiente paso";
+	private JComboBox<String> comboBoxTamaño;
+	private JPanel panelPuzzle;
+	private JLabel Movimientos;
+
+	public InterfazPuzzle() {
+		setSize(600, 600);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.NORTH);
+		panel.setLayout(new GridLayout(0, 4, 0, 0));
+
+		JLabel LabelTamaño = new JLabel("Tama\u00F1o");
+		panel.add(LabelTamaño);
+
+		comboBoxTamaño = new JComboBox<String>();
+		comboBoxTamaño.setModel(new DefaultComboBoxModel<String>(new String[] { "", "3x3", "4x4", "5x5", "6x6" }));
+		comboBoxTamaño.setActionCommand(GENERAR);
+		comboBoxTamaño.addActionListener(this);
+		panel.add(comboBoxTamaño);
+
+		panel.add(new JLabel(""));
+
+		JButton btnGenerar = new JButton("Generar");
+		btnGenerar.setActionCommand(GENERAR_PROBLEMA);
+		btnGenerar.addActionListener(this);
+		panel.add(btnGenerar);
+		
+		panel.add(new JLabel(""));
+		panel.add(new JLabel(""));
+		panel.add(new JLabel(""));
+
+		JButton btnSiguientePaso = new JButton("Siguiente paso");
+		btnSiguientePaso.setActionCommand(NEXTSTEP);
+		btnSiguientePaso.addActionListener(this);
+		panel.add(btnSiguientePaso);
+
+		panelPuzzle = new JPanel();
+		getContentPane().add(panelPuzzle, BorderLayout.CENTER);
+		JPanel panel_2 = new JPanel();
+		getContentPane().add(panel_2, BorderLayout.SOUTH);
+		panel_2.setLayout(new GridLayout(1, 0, 0, 0));
+
+	
+		panel_2.add(new JLabel(""));
+		panel_2.add(new JLabel(""));
+		panel_2.add(new JLabel(""));
+		panel_2.add(new JLabel(""));
+
+		JLabel LabelMovimientos = new JLabel("movimientos:");
+		LabelMovimientos.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		panel_2.add(LabelMovimientos);
+
+		Movimientos = new JLabel("0");
+		panel_2.add(Movimientos);
+
+	}
+
+	private void generarProblema() {
+		try {
+			puzzle = new Puzzle2(n);
+			panelPuzzle.removeAll();
+			panelPuzzle.setLayout(new GridLayout(n, n));
+			cicloGenerador(n, puzzle.getProblema());
+			Movimientos.setText(Integer.toString(puzzle.getMovimientos()));
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(this, "Por favor seleccione tamaño");
+		}
+	}
+
+	private void generarPuzzle() {
+		panelPuzzle.removeAll();
+		if (comboBoxTamaño.getSelectedItem().equals("3x3")) {
+			n = 3;
+			int[][] solucion = Puzzle2.MakeSolution(n);
+			cicloGenerador(n, solucion);
+			Movimientos.setText("0");
+		} else if (comboBoxTamaño.getSelectedItem().equals("4x4")) {
+			n = 4;
+			int[][] solucion = Puzzle2.MakeSolution(n);
+			cicloGenerador(n, solucion);
+			Movimientos.setText("0");
+		} else if (comboBoxTamaño.getSelectedItem().equals("5x5")) {
+			n = 5;
+			int[][] solucion = Puzzle2.MakeSolution(n);
+			cicloGenerador(n, solucion);
+			Movimientos.setText("0");
+		} else if (comboBoxTamaño.getSelectedItem().equals("6x6")) {
+			n = 6;
+			int[][] solucion = Puzzle2.MakeSolution(n);
+			cicloGenerador(n, solucion);
+			Movimientos.setText("0");
+		}
+	}
+
+	private void cicloGenerador(int n, int[][] matriz) {
+		panelPuzzle.setLayout(new GridLayout(n, n));
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (matriz[i][j] == 0) {
+					panelPuzzle.add(new ImagenVacio());
+				} else if (matriz[i][j] == 1) {
+					panelPuzzle.add(new Imagen1());
+				} else if (matriz[i][j] == 2) {
+					panelPuzzle.add(new Imagen2());
+				} else if (matriz[i][j] == 3) {
+					panelPuzzle.add(new Imagen3());
+				} else if (matriz[i][j] == 4) {
+					panelPuzzle.add(new Imagen4());
+				} else if (matriz[i][j] == 5) {
+					panelPuzzle.add(new Imagen5());
+				} else if (matriz[i][j] == 6) {
+					panelPuzzle.add(new Imagen6());
+				} else if (matriz[i][j] == 7) {
+					panelPuzzle.add(new Imagen7());
+				} else if (matriz[i][j] == 8) {
+					panelPuzzle.add(new Imagen8());
+				} else if (matriz[i][j] == 9) {
+					panelPuzzle.add(new Imagen9());
+				} else if (matriz[i][j] == 10) {
+					panelPuzzle.add(new Imagen10());
+				} else if (matriz[i][j] == 11) {
+					panelPuzzle.add(new Imagen11());
+				} else if (matriz[i][j] == 12) {
+					panelPuzzle.add(new Imagen12());
+				} else if (matriz[i][j] == 13) {
+					panelPuzzle.add(new Imagen13());
+				} else if (matriz[i][j] == 14) {
+					panelPuzzle.add(new Imagen14());
+				} else if (matriz[i][j] == 15) {
+					panelPuzzle.add(new Imagen15());
+				} else if (matriz[i][j] == 16) {
+					panelPuzzle.add(new Imagen16());
+				} else if (matriz[i][j] == 17) {
+					panelPuzzle.add(new Imagen17());
+				} else if (matriz[i][j] == 18) {
+					panelPuzzle.add(new Imagen18());
+				} else if (matriz[i][j] == 19) {
+					panelPuzzle.add(new Imagen19());
+				} else if (matriz[i][j] == 20) {
+					panelPuzzle.add(new Imagen20());
+				} else if (matriz[i][j] == 21) {
+					panelPuzzle.add(new Imagen21());
+				} else if (matriz[i][j] == 22) {
+					panelPuzzle.add(new Imagen22());
+				} else if (matriz[i][j] == 23) {
+					panelPuzzle.add(new Imagen23());
+				} else if (matriz[i][j] == 24) {
+					panelPuzzle.add(new Imagen24());
+				} else if (matriz[i][j] == 25) {
+					panelPuzzle.add(new Imagen25());
+				} else if (matriz[i][j] == 26) {
+					panelPuzzle.add(new Imagen26());
+				} else if (matriz[i][j] == 27) {
+					panelPuzzle.add(new Imagen27());
+				} else if (matriz[i][j] == 28) {
+					panelPuzzle.add(new Imagen28());
+				} else if (matriz[i][j] == 29) {
+					panelPuzzle.add(new Imagen29());
+				} else if (matriz[i][j] == 30) {
+					panelPuzzle.add(new Imagen30());
+				} else if (matriz[i][j] == 31) {
+					panelPuzzle.add(new Imagen31());
+				} else if (matriz[i][j] == 32) {
+					panelPuzzle.add(new Imagen32());
+				} else if (matriz[i][j] == 33) {
+					panelPuzzle.add(new Imagen33());
+				} else if (matriz[i][j] == 34) {
+					panelPuzzle.add(new Imagen34());
+				} else if (matriz[i][j] == 35) {
+					panelPuzzle.add(new Imagen35());
+				}
+
+				panelPuzzle.updateUI();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		InterfazPuzzle puzzle = new InterfazPuzzle();
+		puzzle.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		String comando = arg0.getActionCommand();
+		// generara un puzzle ordenado
+		if (comando.equals(GENERAR)) {
+			generarPuzzle();
+		}
+		if (comando.equals(NEXTSTEP)) {
+			try {
+				if (!puzzle.getPasos().isEmpty()) {
+					siguientePaso();
+				} else {
+					if (JOptionPane.showConfirmDialog(this, "ya esta soluciondo el puzzle ¿Desea generar otro?",
+							"Puzzle", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+						generarProblema();
+					}
+				}
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(this, "Genere primero el Puzzle");
+			}
+		}
+		if (comando.equals(GENERAR_PROBLEMA)) {
+			generarProblema();
+		}
+	}
+
+	private void siguientePaso() {
+		panelPuzzle.removeAll();
+		int[][] paso = puzzle.getPasos().pop().getInfo();
+		panelPuzzle.setLayout(new GridLayout(n, n));
+		cicloGenerador(n, paso);
+	}
+}
